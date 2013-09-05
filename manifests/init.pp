fdb0d29456ce540529daee9d5d807da602384186
nervo <nervo@nervo.net>
2013-09-05 13:45:29 +0200

    Add stdlib requirement

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,11 @@
-class dotdeb(
+class dotdeb (
   $key   = 'http://www.dotdeb.org/dotdeb.gpg',
   $php54 = false,
   $php55 = false
 ) {
+
+    require stdlib
+
     class { 'dotdeb::install':
     } -> anchor { 'dotdeb::end': }
 }
