2dd2f6ecb6750164d5ca5ed7ac1e15b2bef82dae
nervo <nervo@nervo.net>
2013-09-05 13:33:41 +0200

    Update for wheezy

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
-class dotdeb (
-  $key           = 'http://www.dotdeb.org/dotdeb.gpg',
-  $squeeze_php54 = false,
-  $wheezy_php55  = false
+class dotdeb(
+  $key   = 'http://www.dotdeb.org/dotdeb.gpg',
+  $php54 = false,
+  $php55 = false
 ) {
     class { 'dotdeb::install':
     } -> anchor { 'dotdeb::end': }
